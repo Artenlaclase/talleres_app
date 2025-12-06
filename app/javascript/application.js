449c3567861144ee7239dce799bf448c499f43d7
Artenlaclase <artenlaclase@gmail.com>
2025-12-06 18:11:07 -0300

    Implement responsive navbar with toggle functionality for mobile menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Menú responsive (toggle)
+document.addEventListener("turbo:load", () => {
+	const btn = document.getElementById("menu-btn");
+	const menu = document.getElementById("mobile-menu");
+
+	if (btn && menu) {
+		btn.addEventListener("click", () => {
+			menu.classList.toggle("hidden");
+		});
+	}
+});
