da050595b7140fb1d42572a5d75a16784d4ff672
Artenlaclase <artenlaclase@gmail.com>
2025-12-06 18:18:54 -0300

    chore: add package-lock.json with initial dependencies for Tailwind CSS and related packages

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,7 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+// Ensure menu controller is registered explicitly
+import MenuController from "controllers/menu_controller"
+application.register("menu", MenuController)
